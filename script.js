6d6ef8e692dff70c846671228071b29b0001e20c
sabrinaferrira03-ui <sabrinaferrira03@gmail.com>
2025-10-30 12:53:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Opcional: Adicione scripts de interatividade aqui mais tarde.
+
+// Exemplo simples para o futuro:
+// window.onload = function() {
+//     console.log("Sabrina Cremes: Site carregado!");
+// };
